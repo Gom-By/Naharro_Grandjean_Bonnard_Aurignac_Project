13f76994e7370b6e94c791f8fbffe263c84b77b4
Nathan <bonnard.nathan.snt@gmail.com>
2025-09-24 19:52:25 +0200

    Limite spawn ennemis + Limite position joueur

--- invaders/Scripts/Singleton/game_manager.gd
+++ invaders/Scripts/Singleton/game_manager.gd
@@ -11,9 +11,6 @@ func _ready() -> void:
 	add_child(timer)
 	timer.timeout.connect(spawn_enemy)
 
-func _process(_delta: float) -> void:
-	pass
-
 func spawn_enemy() -> void:
 	update_enemies()
 	print_debug("enemies :", enemies)
@@ -21,7 +18,11 @@ func spawn_enemy() -> void:
 	
 	var enemy: Node2D = pre_enemy.instantiate()
 	get_tree().current_scene.call_deferred("add_child", enemy)
-	var pos_x := randf_range(0, get_window().size.x)
+	
+	var spawn_path: PathFollow2D = get_tree().current_scene.get_node("SpawnPath/PathFollow2D")
+	spawn_path.progress_ratio = randf()
+	var pos_x: float = spawn_path.position.x
+	
 	enemy.global_position = Vector2(pos_x, 0);
 	enemies.append(enemy)
 
